d164494dbb6d9347509baf2cecaec0324cd62546
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-05-21 17:23:48 +0300

    Only whitelist certain errors

--- driver/pgdriver/error.go
+++ driver/pgdriver/error.go
@@ -8,15 +8,22 @@ import (
 // Error represents an error returned by PostgreSQL server
 // using PostgreSQL ErrorResponse protocol.
 //
-// https://www.postgresql.org/docs/10/static/protocol-message-formats.html
+// https://www.postgresql.org/docs/current/static/protocol-message-formats.html
 type Error struct {
 	m map[byte]string
 }
 
+// Field returns a string value associated with an error field.
+//
+// https://www.postgresql.org/docs/current/static/protocol-error-fields.html
 func (err Error) Field(k byte) string {
 	return err.m[k]
 }
 
+// IntegrityViolation reports whether an error is a part of
+// Integrity Constraint Violation class of errors.
+//
+// https://www.postgresql.org/docs/current/static/errcodes-appendix.html
 func (err Error) IntegrityViolation() bool {
 	switch err.Field('C') {
 	case "23000", "23001", "23502", "23503", "23505", "23514", "23P01":
@@ -35,8 +42,8 @@ func isBadConn(err error, allowTimeout bool) bool {
 	if err == nil {
 		return false
 	}
-	if pgErr, ok := err.(Error); ok {
-		return pgErr.Field('S') == "FATAL"
+	if err, ok := err.(Error); ok {
+		return !err.IntegrityViolation()
 	}
 	if allowTimeout {
 		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
