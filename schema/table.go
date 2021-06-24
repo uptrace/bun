b4392cac5c407a578111bf932d52479443f649fd
rojan-dinc <rojan.dinc@schibsted.com>
2021-06-24 23:19:12 +0200

    Add warn log if field is zero valued time

--- schema/table.go
+++ schema/table.go
@@ -126,6 +126,10 @@ func (t *Table) CheckPKs() error {
 }
 
 func (t *Table) addField(field *Field) {
+	if field.Type == timeType && !field.NullZero {
+		internal.Warn.Printf("the field %s has zero value", field.Name)
+	}
+
 	t.Fields = append(t.Fields, field)
 	if field.IsPK {
 		t.PKs = append(t.PKs, field)
