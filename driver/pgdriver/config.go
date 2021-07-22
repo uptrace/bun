318c89381eb4efb12afb4090d48db4ca2ed104d8
Davood Falahati <falahati.davood@gmail.com>
2021-07-22 16:24:02 +0430

    sentences are better not to be inserted within error messages. Thus, we are taking steps the users to submit the connection credentials with WITH* clauses

--- driver/pgdriver/config.go
+++ driver/pgdriver/config.go
@@ -73,6 +73,9 @@ func WithTLSConfig(cfg *tls.Config) DriverOption {
 }
 
 func WithUser(user string) DriverOption {
+	if user == "" {
+		panic("user is empty")
+	}
 	return func(d *driverConnector) {
 		d.cfg.User = user
 	}
@@ -208,10 +211,7 @@ func env(key, defValue string) string {
 // it can be extended to check other parameters
 func (c *Config) verify() error {
 	if c.User == "" {
-		return errors.New(
-			`pgdriver: username is required. 
-		 	 Check your config and make sure User is not left blank.`,
-		)
+		return errors.New("pgdriver: User option is empty (to configure, use WithUser).")
 	}
 	return nil
 }
