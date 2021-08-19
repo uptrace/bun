55a82e6298e6c898e95c39381de2a4d50b4f1639
Davood Falahati <falahati.davood@gmail.com>
2021-08-19 16:15:15 +0430

    buntypes added, bigint is placed inside buntypes

---
+++ buntypes/bigint/README.md
@@ -0,0 +1,94 @@
+
+**bigint** is a wrapper around math/big package to let us use big.int type in bun
+
+Disclaimer: math/big does not implement database/sql scan/value methods and it can't be used in bun. This package uses math/big in its heart and extends its usefulness even into postgres.
+
+## Example use with bun
+
+**go-pg** is an amazing orm for gophers to utilize postgres. This package is used to help **go-pg** users implement **math/big** functionalities.
+
+```
+
+	type TableWithBigint struct {
+		Id        uint64
+		Name      string
+		Deposit   *bigint.Bigint
+	}
+
+```
+
+### Mathematical Operations: 
+
+This package supports basic mathematical operations such as addition, subtraction, division, negation etc.
+
+Example : 
+
+```
+	x := bigint.FromInt64(100)
+	y , err := bigint.FromString("9999999999999999999999999999999999999999")
+
+	if err!=nil {
+		panic(err)
+	}
+
+	y.Add(x) // 9999999999999999999999999999999999999999 + 100
+	y.Sub(x) // 9999999999999999999999999999999999999999 - 100
+	y.Neg() //  -9999999999999999999999999999999999999999
+	// on the fly operation
+	c:= biginit.FromInt64(100).Mul(y) // 100 * 100 = 10000
+	c.Div(x) // 10000/100 = 100
+	c.Neg().Abs() // |-10000| = 10000
+
+```
+
+For extracting math/big's bigint you can simply do as follows:
+
+```
+    d:= bigint.ToMathBig(x)
+```
+
+Now you can do your calculations and convert it back to buntypes/bigint with:
+
+```
+   x = bigint.FromBigint(d)
+```
+
+### comparisons:
+
+let we have x , y as two bigint.Bigint numbers in buntypes. 
+```
+   x:= bigint.FromInt64(100)
+   y:= bigint.FromInt64(90)
+```
+
+For comparing the above numbers, we can do as follow:
+
+```
+   cmp:=x.Cmp(y)
+```
+
+**cmp** has the following methods: 
+
+```
+    // equal
+	Eq() bool
+	// greater than
+	Gt() bool
+	// lower than
+	Lt() bool
+	// Greater or equal
+	Geq() bool
+	// Lower or equal
+	Leq() bool
+
+```
+
+Thus, we have the following results:
+
+```
+  x.Cmp(y).Eq() // 100 == 90 : false
+  x.Cmp(y).Geq() // 100 >= 90 : true
+  x.Cmp(y).Gt() // 100 > 90 : true
+  x.Cmp(y).Lt() // 100 < 90 : false
+  x.Cmp(y).Leq() // 100 <= 90 : false
+```
---
+++ buntypes/bigint/bigint_test.go
@@ -0,0 +1,105 @@
+package bigint_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/uptrace/bun/buntypes/bigint"
+)
+
+func TestMathOperations(t *testing.T) {
+	a := big.NewInt(100)
+	b := big.NewInt(200)
+
+	t.Run("multiply", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		y := bigint.FromMathBig(b)
+		// 100 * 200 = 20000
+		assert.Equal(t, bigint.FromMathBig(big.NewInt(20000)), x.Mul(y))
+	})
+	t.Run("add", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		y := bigint.FromMathBig(b)
+		// 100 + 200 = 300
+		assert.Equal(t, bigint.FromMathBig(big.NewInt(300)), x.Add(y))
+	})
+
+	t.Run("sub", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		y := bigint.FromMathBig(b)
+		// 100 -200 = -100
+		assert.Equal(t, bigint.FromMathBig(big.NewInt(-100)), x.Sub(y))
+	})
+
+	t.Run("div", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		y := bigint.FromMathBig(b)
+		// 200 / 100 = 2
+		assert.Equal(t, bigint.FromMathBig(big.NewInt(2)), y.Div(x))
+	})
+
+	t.Run("negation", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		assert.Equal(t, bigint.FromMathBig(big.NewInt(-100)), x.Neg())
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		assert.Equal(t, int64(-100), x.Neg().ToInt64())
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		assert.Equal(t, uint64(100), x.ToUInt64())
+	})
+	t.Run("toString", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+		assert.Equal(t, "100", x.String())
+	})
+	t.Run("fromString", func(t *testing.T) {
+		x, err := bigint.FromString("100")
+		assert.Nil(t, err)
+		assert.Equal(t, "100", x.String())
+	})
+	t.Run("fromInt64", func(t *testing.T) {
+		x := bigint.FromInt64(100000000)
+		assert.Equal(t, int64(100000000), x.ToInt64())
+	})
+
+	t.Run("Abs", func(t *testing.T) {
+		x := bigint.FromMathBig(a)
+
+		assert.Equal(t, x.Neg().Abs(), x)
+	})
+	t.Run("compare: ", func(t *testing.T) {
+		x := bigint.FromMathBig(a) // 100
+		y := bigint.FromMathBig(b) // 200
+
+		cmp := x.Cmp(y)
+
+		t.Run("eq ?", func(t *testing.T) {
+			assert.Equal(t, cmp.Eq(), false)
+		})
+		t.Run("lt ?", func(t *testing.T) {
+			assert.Equal(t, cmp.Lt(), true)
+		})
+		t.Run("gt ?", func(t *testing.T) {
+			assert.Equal(t, cmp.Gt(), false)
+		})
+		t.Run("leq ?", func(t *testing.T) {
+			assert.Equal(t, cmp.Leq(), true)
+		})
+		t.Run("geq ?", func(t *testing.T) {
+			assert.Equal(t, cmp.Geq(), false)
+		})
+	})
+
+	t.Run("empty string ", func(t *testing.T) {
+
+		x, err := bigint.FromString("")
+		assert.Nil(t, err)
+		assert.Equal(t, x.ToInt64(), int64(0))
+	})
+
+}
