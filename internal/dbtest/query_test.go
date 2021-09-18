27c477ce071d4c49c99a2531d638ed9f20e33461
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-09-18 11:57:57 +0300

    fix: don't append soft delete where for insert queries with on conflict clause

--- internal/dbtest/query_test.go
+++ internal/dbtest/query_test.go
@@ -551,6 +551,9 @@ func TestQuery(t *testing.T) {
 			}
 			return db.NewInsert().Model(new(Model))
 		},
+		func(db *bun.DB) schema.QueryAppender {
+			return db.NewInsert().Model(new(SoftDelete)).On("CONFLICT DO NOTHING")
+		},
 	}
 
 	timeRE := regexp.MustCompile(`'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+(\+\d{2}:\d{2})?'`)
