6c676ce13f05fe763471fbec2d5a2db48bc88650
Leo Farrell <lfarrell@au1.ibm.com>
2022-07-10 19:04:13 +1000

    feat: create an extra module for newrelic (#599)
    * feat: create an extra module for newrelic

---
+++ extra/bunrelic/go.mod
@@ -0,0 +1,23 @@
+module github.com/uptrace/bun/extra/bunrelic
+
+go 1.17
+
+replace github.com/uptrace/bun => ../..
+
+require (
+	github.com/newrelic/go-agent/v3 v3.15.2
+	github.com/uptrace/bun v1.1.5
+)
+
+require (
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.27.0 // indirect
+)
---
+++ extra/bunrelic/relic.go
@@ -0,0 +1,51 @@
+package bunrelic
+
+import (
+	"context"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/newrelic/go-agent/v3/newrelic/sqlparse"
+	"github.com/uptrace/bun"
+)
+
+type QueryHook struct {
+	baseSegment newrelic.DatastoreSegment
+}
+
+var _ bun.QueryHook = (*QueryHook)(nil)
+
+type nrBunCtxKey string
+
+const nrBunSegmentKey nrBunCtxKey = "nrbunsegment"
+
+// NewQueryHook creates a new bun.QueryHook which reports database usage
+// information to new relic.
+func NewQueryHook(options ...Option) *QueryHook {
+	h := &QueryHook{}
+	for _, o := range options {
+		o(h)
+	}
+	return h
+}
+
+func (q *QueryHook) BeforeQuery(ctx context.Context, qe *bun.QueryEvent) context.Context {
+	segment := q.baseSegment
+
+	if qe.Model != nil {
+		if t, ok := qe.Model.(bun.TableModel); ok {
+			segment.Operation = qe.Operation()
+			segment.Collection = t.Table().Name
+		} else {
+			sqlparse.ParseQuery(&segment, qe.Query)
+		}
+	} else {
+		sqlparse.ParseQuery(&segment, qe.Query)
+	}
+	segment.StartTime = newrelic.FromContext(ctx).StartSegmentNow()
+	return context.WithValue(ctx, nrBunSegmentKey, &segment)
+
+}
+func (q *QueryHook) AfterQuery(ctx context.Context, qe *bun.QueryEvent) {
+	segment := ctx.Value(nrBunSegmentKey).(*newrelic.DatastoreSegment)
+	segment.End()
+}
