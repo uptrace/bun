67e4412a972a9ed5f3a1d07c66957beedbc8a8a3
Bryan <ImSingee@users.noreply.github.com>
2022-11-29 10:26:19 +0000

    fix: prevent panic when use pg array with custom database type

--- util.go
+++ util.go
@@ -89,12 +89,12 @@ func makeSliceNextElemFunc(v reflect.Value) func() reflect.Value {
 				if elem.IsNil() {
 					elem.Set(reflect.New(elemType))
 				}
-				return elem.Elem()
+				return elem
 			}
 
 			elem := reflect.New(elemType)
 			v.Set(reflect.Append(v, elem))
-			return elem.Elem()
+			return elem
 		}
 	}
 
