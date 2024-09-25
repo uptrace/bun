c070026837880501369559f763b7bce4578aff6e
Mikhail Chekh <m4ex@users.noreply.github.com>
2024-09-25 09:02:41 +0300

    Add option to initialize MySQLDialect with server timezone (#1021)
    * mysqldialect: add time location support
    
    * method chaining setter
    
    * use functional Options Pattern

--- dialect/mysqldialect/dialect.go
+++ dialect/mysqldialect/dialect.go
@@ -27,14 +27,17 @@ func init() {
 	}
 }
 
+type DialectOption func(d *Dialect)
+
 type Dialect struct {
 	schema.BaseDialect
 
 	tables   *schema.Tables
 	features feature.Feature
+	loc      *time.Location
 }
 
-func New() *Dialect {
+func New(opts ...DialectOption) *Dialect {
 	d := new(Dialect)
 	d.tables = schema.NewTables(d)
 	d.features = feature.AutoIncrement |
@@ -47,7 +50,22 @@ func New() *Dialect {
 		feature.InsertOnDuplicateKey |
 		feature.SelectExists |
 		feature.CompositeIn
+
+	for _, opt := range opts {
+		opt(d)
+	}
+
 	return d
+}
+
+func WithTimeLocation(loc string) DialectOption {
+	return func(d *Dialect) {
+		location, err := time.LoadLocation(loc)
+		if err != nil {
+			panic(fmt.Errorf("mysqldialect can't load provided location %s: %s", loc, err))
+		}
+		d.loc = location
+	}
 }
 
 func (d *Dialect) Init(db *sql.DB) {
@@ -103,9 +121,13 @@ func (d *Dialect) IdentQuote() byte {
 	return '`'
 }
 
-func (*Dialect) AppendTime(b []byte, tm time.Time) []byte {
+func (d *Dialect) AppendTime(b []byte, tm time.Time) []byte {
 	b = append(b, '\'')
-	b = tm.AppendFormat(b, "2006-01-02 15:04:05.999999")
+	if d.loc != nil {
+		b = tm.In(d.loc).AppendFormat(b, "2006-01-02 15:04:05.999999")
+	} else {
+		b = tm.AppendFormat(b, "2006-01-02 15:04:05.999999")
+	}
 	b = append(b, '\'')
 	return b
 }
