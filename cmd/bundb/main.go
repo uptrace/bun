c5f3d9146793b9a49790a750ff6ef086f6095a3f
dyma solovei <dmytro.y.solovei@gmail.com>
2024-11-19 15:00:09 +0100

    feat: manage database schema with bundb CLI
    Here we introduce bundb auto migrate command to create and run migrations from the CLI.
    It works by importing the Models from a pre-build user plugin, similarly to how Migrator
    gets the list of migrations by being compiled together with the user's package.
    The difference here is that bundb can be distributed as a standalone binary and only requires
    users to set up the migrations/main.go file.

---
+++ cmd/bundb/main.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"plugin"
+	"strings"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mssqldialect"
+	"github.com/uptrace/bun/dialect/mysqldialect"
+	"github.com/uptrace/bun/dialect/oracledialect"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/dialect/sqlitedialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/driver/sqliteshim"
+	"github.com/uptrace/bun/migrate"
+	"github.com/uptrace/bun/schema"
+	"github.com/urfave/cli/v2"
+)
+
+const defaultMigrationsDirectory = "./migrations/"
+
+var (
+	supportedDrivers = []string{"postgres", "sqlserver", "mysql", "oci8", "file"}
+
+	// TODO: build automatically based on the migrationsPath
+	pluginPath string
+
+	// Migrator options
+	migratorOptions  []migrate.MigratorOption
+	migrationOptions []migrate.MigrationOption
+
+	// AutoMigrator options
+	autoMigratorOptions []migrate.AutoMigratorOption
+)
+
+var app = &cli.App{
+	Name: "bundb",
+	Commands: cli.Commands{
+		// bundb init --create-directory
+		// bundb create --sql --go --tx [-d | --dir]
+		// bundb migrate
+		// bundb auto create --sql --tx
+		// bundb auto migrate
+		&cli.Command{
+			Name:  "auto",
+			Usage: "manage database schema with AutoMigrator",
+			Subcommands: cli.Commands{
+				&cli.Command{
+					Name:  "migrate",
+					Usage: "Generate SQL migrations and apply them right away",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:     "uri",
+							Aliases:  []string{"database-uri", "dsn"},
+							Required: true,
+							EnvVars:  []string{"BUNDB_URI"},
+						},
+						&cli.StringFlag{
+							Name: "driver",
+						},
+						&cli.StringFlag{
+							Name:        "plugin",
+							Destination: &pluginPath,
+							Required:    true,
+						},
+						&cli.StringFlag{
+							Name:    "d",
+							Aliases: []string{"migrations-directory"},
+							Action: func(ctx *cli.Context, dir string) error {
+								autoMigratorOptions = append(autoMigratorOptions, migrate.WithMigrationsDirectoryAuto(dir))
+								return nil
+							},
+						},
+						&cli.StringFlag{
+							Name:    "t",
+							Aliases: []string{"migrations-table"},
+							Action: func(ctx *cli.Context, migrationsTable string) error {
+								autoMigratorOptions = append(autoMigratorOptions, migrate.WithTableNameAuto(migrationsTable))
+								return nil
+							},
+						},
+						&cli.StringFlag{
+							Name:    "l",
+							Aliases: []string{"locks", "migration-locks-table"},
+							Action: func(ctx *cli.Context, locksTable string) error {
+								autoMigratorOptions = append(autoMigratorOptions, migrate.WithLocksTableNameAuto(locksTable))
+								return nil
+							},
+						},
+						&cli.StringFlag{
+							Name:    "s",
+							Aliases: []string{"schema"},
+							Action: func(ctx *cli.Context, schemaName string) error {
+								autoMigratorOptions = append(autoMigratorOptions, migrate.WithSchemaName(schemaName))
+								return nil
+							},
+						},
+						&cli.StringSliceFlag{
+							Name: "exclude",
+							Action: func(ctx *cli.Context, tables []string) error {
+								autoMigratorOptions = append(autoMigratorOptions, migrate.WithExcludeTable(tables...))
+								return nil
+							},
+						},
+					},
+					Action: func(ctx *cli.Context) error {
+						db, err := connect(ctx.String("uri"), ctx.String("driver"), !ctx.IsSet("driver"))
+						if err != nil {
+							return err
+
+						}
+
+						if !ctx.IsSet("migrations-directory") {
+							autoMigratorOptions = append(autoMigratorOptions, migrate.WithMigrationsDirectoryAuto(defaultMigrationsDirectory))
+
+						}
+						m, err := automigrator(db)
+						if err != nil {
+							return err
+						}
+
+						group, err := m.Migrate(ctx.Context)
+						if err != nil {
+							return err
+						}
+						if group.IsZero() {
+							log.Print("ok, nothing to migrate")
+						}
+						return nil
+
+					},
+				},
+			},
+		},
+	},
+}
+
+// connect to the database under the URI. A driver must be one of the supported drivers.
+// If not set explicitly, the name of the driver is guessed from the URI.
+//
+// Example:
+//
+//	"postgres://postgres:@localhost:5432/postgres" -> "postegres"
+func connect(uri, driverName string, guessDriver bool) (*bun.DB, error) {
+	var sqldb *sql.DB
+	var dialect schema.Dialect
+	var err error
+
+	if guessDriver {
+		driver, _, found := strings.Cut(uri, ":")
+		if !found {
+			return nil, fmt.Errorf("driver cannot be guessed from connection string; pass --driver option explicitly")
+		}
+		driverName = driver
+	}
+
+	switch driverName {
+	case "postgres":
+		sqldb = sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(uri)))
+		dialect = pgdialect.New()
+	case "sqlserver":
+		sqldb, err = sql.Open(driverName, uri)
+		dialect = mssqldialect.New()
+	case "file":
+		sqldb, err = sql.Open(sqliteshim.ShimName, uri)
+		dialect = sqlitedialect.New()
+	case "mysql":
+		sqldb, err = sql.Open(driverName, uri)
+		dialect = mysqldialect.New()
+	case "oci8":
+		sqldb, err = sql.Open(driverName, uri)
+		dialect = oracledialect.New()
+	default:
+		err = fmt.Errorf("driver %q not recognized, supported drivers are %v", driverName, supportedDrivers)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bun.NewDB(sqldb, dialect), nil
+}
+
+// automigrator creates AutoMigrator for models from user's 'migrations' package.
+func automigrator(db *bun.DB) (*migrate.AutoMigrator, error) {
+	sym, err := lookup("Models")
+	if err != nil {
+		return nil, err
+	}
+
+	models, ok := sym.(*[]interface{})
+	if !ok {
+		return nil, fmt.Errorf("migrations plugin must export Models as []interface{}, got %T", models)
+	}
+	autoMigratorOptions = append(autoMigratorOptions, migrate.WithModel(*models...))
+
+	auto, err := migrate.NewAutoMigrator(db, autoMigratorOptions...)
+	if err != nil {
+		return nil, err
+	}
+	return auto, nil
+}
+
+// lookup a symbol from user's migrations plugin.
+func lookup(symbol string) (plugin.Symbol, error) {
+	p, err := plugin.Open(pluginPath)
+	if err != nil {
+		return nil, err
+	}
+	return p.Lookup(symbol)
+}
+
+func main() {
+	log.SetPrefix("bundb: ")
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,17 +8,46 @@ require (
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.8.1
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc
+	github.com/uptrace/bun/dialect/mssqldialect v1.2.6-0.20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/dialect/mysqldialect v1.2.6-0.20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/dialect/oracledialect v0.0.0-20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/dialect/pgdialect v1.2.6-0.20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/dialect/sqlitedialect v1.2.6-0.20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/driver/pgdriver v1.2.6-0.20241118024642-c6b302ff06f6
+	github.com/uptrace/bun/driver/sqliteshim v1.2.5
+	github.com/urfave/cli/v2 v2.27.5
 	github.com/vmihailenco/msgpack/v5 v5.4.1
 )
 
 require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	mellium.im/sasl v0.3.2 // indirect
+	modernc.org/gc/v3 v3.0.0-20241004144649-1aea3fae8852 // indirect
+	modernc.org/libc v1.61.0 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/sqlite v1.33.1 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
 )
