1376d1870bfe3d89e3630203787f1e87c503d5df
Vadim Voitenko <wwoytenko@gmail.com>
2024-12-03 18:31:16 +0200

    feat: enhance debugging by adding query comments in headers

---
+++ util_test.go
@@ -0,0 +1,41 @@
+package bun
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_appendComment(t *testing.T) {
+	t.Run("ordinary comment", func(t *testing.T) {
+		var res []byte
+		c := "comment"
+
+		s := appendComment(res, c)
+		require.Equal(t, "/* comment */ ", string(s))
+	})
+
+	t.Run("only open sequence", func(t *testing.T) {
+		var res []byte
+		c := "/* comment"
+
+		s := appendComment(res, c)
+		require.Equal(t, "/* /\\* comment */ ", string(s))
+	})
+
+	t.Run("only close sequence", func(t *testing.T) {
+		var res []byte
+		c := "comment */"
+
+		s := appendComment(res, c)
+		require.Equal(t, "/* comment *\\/ */ ", string(s))
+	})
+
+	t.Run("open and close sequences", func(t *testing.T) {
+		var res []byte
+		c := "/* comment */"
+
+		s := appendComment(res, c)
+		require.Equal(t, "/* /\\* comment *\\/ */ ", string(s))
+	})
+}
