e54fb8ad75368cad0d01cbdd9ff4b5541e3e37cc
Jeffrey Walter <jeffreydwalter@gmail.com>
2025-03-25 14:59:36 -0500

    clean-array-support: fix: remove unused function

--- internal/underscore.go
+++ internal/underscore.go
@@ -63,64 +63,6 @@ func Underscore(s string) string {
 	return string(r)
 }
 
-// processWord handles a single word without underscores
-func processWord(s string) string {
-	if len(s) == 0 {
-		return s
-	}
-
-	r := make([]byte, 0, len(s)+5)
-
-	// Track uppercase sequences for acronym detection
-	inUpperSequence := false
-
-	// Track if we're at the beginning of a new word
-	atWordStart := true
-
-	for i := 0; i < len(s); i++ {
-		c := s[i]
-
-		if IsUpper(c) {
-			// Handle single uppercase letter at the end (ItemsP -> itemsp)
-			if i > 0 && i == len(s)-1 {
-				r = append(r, ToLower(c))
-				continue
-			}
-
-			// First letter of the string is always lowercase without underscore
-			if atWordStart {
-				r = append(r, ToLower(c))
-				atWordStart = false
-				inUpperSequence = true
-				continue
-			}
-
-			// Handle camelCase transition (lowercase to uppercase)
-			if i > 0 && !inUpperSequence {
-				r = append(r, '_')
-			}
-
-			// Handle acronym transition (uppercase sequence to lowercase)
-			if i+1 < len(s) && IsLower(s[i+1]) && inUpperSequence && i > 0 && i+1 < len(s) {
-				// We're at the end of an acronym like "HTTP" in "HTTPRequest"
-				// Only add underscore if this isn't immediately after another underscore
-				if len(r) > 0 && r[len(r)-1] != '_' {
-					r = append(r, '_')
-				}
-			}
-
-			r = append(r, ToLower(c))
-			inUpperSequence = true
-		} else {
-			r = append(r, c)
-			inUpperSequence = false
-			atWordStart = false
-		}
-	}
-
-	return string(r)
-}
-
 func CamelCased(s string) string {
 	r := make([]byte, 0, len(s))
 	upperNext := true
