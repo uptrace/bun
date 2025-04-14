57cb9d6700d70be26e701cf220170d58c24d84e3
YesToolBox <34413969+yestoolbox@users.noreply.github.com>
2025-04-14 23:49:06 +0800

    Update query_update.go
    // Before (error):
    setCommentFromContext(ctx, query)
    
    // After (working):
    setCommentFromContext[*UpdateQuery](ctx, query)

--- query_update.go
+++ query_update.go
@@ -557,7 +557,7 @@ func (q *UpdateQuery) scanOrExec(
 	}
 
 	// if a comment is propagated via the context, use it
-	setCommentFromContext(ctx, q)
+	setCommentFromContext[*UpdateQuery](ctx, q)
 
 	// Generate the query before checking hasReturning.
 	queryBytes, err := q.AppendQuery(q.db.fmter, q.db.makeQueryBytes())
